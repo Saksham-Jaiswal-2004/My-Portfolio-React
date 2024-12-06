c54d17cba430aec244e3a0ba636c2a1e0e71d9fb
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2024-12-06 23:00:40 +0530

    Updated Many files

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   root: './', // Make sure this is set to the correct root
   build: {
-    outDir: 'build',
     rollupOptions: {
       input: '/index.html',
       external: ['/src/main.jsx'],
