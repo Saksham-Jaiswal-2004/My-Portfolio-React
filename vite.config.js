8d5cba1e885fd5ec3a5c13122abb6e4373c661e8
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2024-12-06 23:42:37 +0530

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/My-Portfolio-React/',
   plugins: [react()],
   root: './', // Make sure this is set to the correct root
   build: {
